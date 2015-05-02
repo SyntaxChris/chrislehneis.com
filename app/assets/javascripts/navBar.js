da3ad743cd9286ede181cd37c1e97731b443eaa4
Chris Lehneis <cdlehneis@gmail.com>
2015-05-02 13:46:44 -0400

    animation complete

--- app/assets/javascripts/navBar.js
+++ app/assets/javascripts/navBar.js
@@ -1,12 +1,101 @@
 $(document).ready(function(){
-	var cntnr = $('.main-cntnr');
+	var container = $('.main-cntnr');
+	var content = $('.cntnt');
+	var containerOpen = false;
 
-	$('.nav-link').on('click', function(){
-		if(cntnr.hasClass('expand')){
-
-			cntnr.addClass('collapse').removeClass('expand');
+	$('.about-link').on('click', function(){
+		if(containerOpen){
+			if($('.about').css('display') === 'block'){
+				content.hide();
+				container.addClass('collapse');
+				container.removeClass('expand');
+				containerOpen = false;
+			}else{
+				content.hide();
+				container.addClass('collapse');
+				container.removeClass('expand');
+				setTimeout(function(){ 
+					container.removeClass('collapse');
+					container.addClass('expand');
+					$('.about').fadeIn();
+				}, 350);
+			}
 		}else{
-			cntnr.addClass('expand');
+			if($('.about').css('display') === 'block'){
+				container.removeClass('collapse');
+				container.addClass('expand');
+			}else{
+				container.removeClass('collapse');
+				container.addClass('expand');
+				content.hide();
+				$('.about').fadeIn();
+			}
+			containerOpen = true;
+		}
+	});
+
+	$('.portfolio-link').on('click', function(){
+		if(containerOpen){
+			if($('.portfolio').css('display') === 'block'){
+				content.hide();
+				container.addClass('collapse');
+				container.removeClass('expand');
+				containerOpen = false;
+			}else{
+				content.hide();
+				container.addClass('collapse');
+				container.removeClass('expand');
+				setTimeout(function(){ 
+					container.removeClass('collapse');
+					container.addClass('expand');
+					$('.portfolio').fadeIn();
+				}, 350);
+			}
+		}else{
+			if($('.portfolio').css('display') === 'block'){
+				container.removeClass('collapse');
+				container.addClass('expand');
+			}else{
+				container.removeClass('collapse');
+				container.addClass('expand');
+				content.hide();
+				$('.portfolio').fadeIn();
+			}
+			containerOpen = true;
+		}
+	});
+
+
+
+
+	$('.contact-link').on('click', function(){
+		if(containerOpen){
+			if($('.contact').css('display') === 'block'){
+				content.hide();
+				container.addClass('collapse');
+				container.removeClass('expand');
+				containerOpen = false;
+			}else{
+				content.hide();
+				container.addClass('collapse');
+				container.removeClass('expand');
+				setTimeout(function(){ 
+					container.removeClass('collapse');
+					container.addClass('expand');
+					$('.contact').fadeIn();
+				}, 350);
+			}
+		}else{
+			if($('.contact').css('display') === 'block'){
+				container.removeClass('collapse');
+				container.addClass('expand');
+			}else{
+				container.removeClass('collapse');
+				container.addClass('expand');
+				content.hide();
+				$('.contact').fadeIn();
+			}
+			containerOpen = true;
 		}
 	});
 });
