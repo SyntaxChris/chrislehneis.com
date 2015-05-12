fa09794acd22871c3e235ecea9dc02038f31a617
Chris Lehneis <cdlehneis@gmail.com>
2015-05-12 00:31:17 -0400

    some mailer config changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {host: "localhost:3000"}
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 25,
@@ -24,7 +25,7 @@ Rails.application.configure do
     enable_starttls_auto: false
   }
   config.action_mailer.raise_delivery_errors = true
-  
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
