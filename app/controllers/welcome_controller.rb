6653f457d6d347310959cc32a4887932ab25f37b
Chris Lehneis <cdlehneis@gmail.com>
2015-05-11 20:51:16 -0400

    uncomments controller handler in create action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,7 +8,7 @@ class WelcomeController < ApplicationController
     
     if message.valid?
       ContactMailer.new_message(message).deliver
-      # redirect_to(root_path, :notice => "Message was successfully sent.")
+      redirect_to(root_path, :notice => "Message was successfully sent.")
     else
     	# Handle this appropriately
 
