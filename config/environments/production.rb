0cb239b01c4b27c240119b0d818186a48f31b155
Chris Lehneis <cdlehneis@gmail.com>
2015-05-12 00:54:42 -0400

    chagnes host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,15 +13,16 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
+  config.action_mailer.default_url_options = { :host => "chrislehneis.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
+    domain: 'chrislehneis.herokuapp.com',
     authentication: "plain",
     user_name: ENV['gmail_user'],
     password: ENV['gmail_password'],
-    enable_starttls_auto: false
+    enable_starttls_auto: true
   }
   config.action_mailer.raise_delivery_errors = true
 
