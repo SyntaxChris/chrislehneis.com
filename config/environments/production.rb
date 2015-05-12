2e7e2ce6d0a0e77f8fa99c6f83b11a6209a31da1
Chris <cdlehneis@gmail.com>
2015-05-12 15:43:59 -0400

    changes production mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,17 +14,17 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.action_mailer.default_url_options = { :host => "192.241.253.23" }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV['production_host'],
-    authentication: "plain",
-    user_name: ENV['gmail_user'],
-    password: ENV['gmail_password'],
-    enable_starttls_auto: true
-  }
-  config.action_mailer.raise_delivery_errors = true
+#  config.action_mailer.delivery_method = :smtp
+ # config.action_mailer.smtp_settings = {
+  #  address: "smtp.gmail.com",
+   # port: 587,
+    #domain: ENV['production_host'],
+   # authentication: "plain",
+   # user_name: ENV['gmail_user'],
+   # password: ENV['gmail_password'],
+   # enable_starttls_auto: true
+ # }
+ # config.action_mailer.raise_delivery_errors = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
